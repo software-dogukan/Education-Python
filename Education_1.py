fd058cdf3bcd8b5becf2931e33022c9de10f10b3
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-08-23 16:00:00 +0300

    Education 1

---
+++ Education_1.py
@@ -0,0 +1,12 @@
+variable_int=5#That variable is integers
+variable_str="I love software"#That variable is string as text
+variable_bool=True#That variable is bool as True or False
+variable_float=3.56#That variable is float as 356/100
+fruits=["Banana","Cherry","Apple"]# That is List as name list
+x,y,z=fruits#Those Variables are writing include from up List
+print(x,y,z)#we write at the screen side by side
+print(x+y+z)#we write at the screen side by side but there aren't space
+print(variable_int+variable_float)#we addition process two variables
+print(variable_int,x)#we write side by side because one variable is integer,another variable is string
+variable_float=variable_float+variable_int#while two variables addition process,same one equalize
+print(variable_float)#we write at the screen
